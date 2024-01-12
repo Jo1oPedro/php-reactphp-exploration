ea9695145ec64988142c28bc2d23953c6be447f7
Jo1oPedro <joao.pedreira@estudante.ufjf.br>
2024-01-12 17:10:45 -0300

    adiciona arquivo com mock de posts

---
+++ src/httpServer/posts.php
@@ -0,0 +1,34 @@
+<?php
+
+return [
+    [
+        'title' => 'cascata1',
+        'body' => 'post do cascata1',
+        'tags' => ['php', 'solid']
+    ],
+    [
+        'title' => 'cascata2',
+        'body' => 'post do cascata2',
+        'tags' => ['react', 'solid']
+    ],
+    [
+        'title' => 'cascata3',
+        'body' => 'post do cascata3',
+        'tags' => ['reactphp', 'solid']
+    ],
+    [
+        'title' => 'cascata4',
+        'body' => 'post do cascata4',
+        'tags' => ['php', 'solid']
+    ],
+    [
+        'title' => 'cascata5',
+        'body' => 'post do cascata5',
+        'tags' => ['php', 'solid']
+    ],
+    [
+        'title' => 'cascata6',
+        'body' => 'post do cascata1',
+        'tags' => ['sql', 'solid']
+    ]
+];
